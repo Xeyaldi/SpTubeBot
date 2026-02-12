999c60d0dea711e49b3af551244b130cd465f4a2
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-02-12 20:45:27 +0400

    Update _filters.py

--- src/utils/_filters.py
+++ src/utils/_filters.py
@@ -124,7 +124,8 @@ class Filter:
             if chat_id is None:
                 return False
 
-            if ApiData(text).is_valid():
+            api = ApiData(text)
+            if api.is_valid() or api.is_save_snap_url():
                 return True
 
             return False if re.match("^https?://", text) else chat_id > 0
